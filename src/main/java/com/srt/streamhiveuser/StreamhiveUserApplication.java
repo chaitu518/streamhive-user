26ab4dfd2843a3979302760a5beeb6ba454dde6c
Naga Sri Sai Chaitanya Kolluri <saichaitanya518@gmail.com>
2025-05-12 15:57:49 +0530

    user consumer for auth service is added

---
+++ src/main/java/com/srt/streamhiveuser/StreamhiveUserApplication.java
@@ -0,0 +1,13 @@
+package com.srt.streamhiveuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamhiveUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamhiveUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/srt/streamhiveuser/StreamhiveUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.srt.streamhiveuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamhiveUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
